cada9c4cf66afadb814cb7848763817ace5a684c
Lamphan23-dr <phannhatlam2306@gmail.com>
2025-02-06 13:02:54 +0700

    Add files via upload

---
+++ Thống kê báo cáo/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Thống_kê_báo_cáo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbTimeFilter = new System.Windows.Forms.ComboBox();
+            this.btnFIller = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgvThongKe = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThongKe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(250, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(305, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thống kê báo cáo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(59, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bộ lọc thời gian";
+            // 
+            // cmbTimeFilter
+            // 
+            this.cmbTimeFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbTimeFilter.FormattingEnabled = true;
+            this.cmbTimeFilter.Items.AddRange(new object[] {
+            "Theo ngày",
+            "Theo tháng",
+            "Theo Năm"});
+            this.cmbTimeFilter.Location = new System.Drawing.Point(65, 118);
+            this.cmbTimeFilter.Name = "cmbTimeFilter";
+            this.cmbTimeFilter.Size = new System.Drawing.Size(299, 39);
+            this.cmbTimeFilter.TabIndex = 2;
+            // 
+            // btnFIller
+            // 
+            this.btnFIller.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFIller.Location = new System.Drawing.Point(334, 175);
+            this.btnFIller.Name = "btnFIller";
+            this.btnFIller.Size = new System.Drawing.Size(121, 62);
+            this.btnFIller.TabIndex = 3;
+            this.btnFIller.Text = "Lọc dữ liệu";
+            this.btnFIller.UseVisualStyleBackColor = true;
+            this.btnFIller.Click += new System.EventHandler(this.btnFIller_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(317, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Biểu đồ danh thu";
+            // 
+            // dgvThongKe
+            // 
+            this.dgvThongKe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvThongKe.Location = new System.Drawing.Point(186, 288);
+            this.dgvThongKe.Name = "dgvThongKe";
+            this.dgvThongKe.RowHeadersWidth = 51;
+            this.dgvThongKe.RowTemplate.Height = 24;
+            this.dgvThongKe.Size = new System.Drawing.Size(439, 150);
+            this.dgvThongKe.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvThongKe);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnFIller);
+            this.Controls.Add(this.cmbTimeFilter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThongKe)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbTimeFilter;
+        private System.Windows.Forms.Button btnFIller;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgvThongKe;
+    }
+}
+
---
+++ Thống kê báo cáo/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Thống_kê_báo_cáo
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Data Source=NHATLAM;Initial Catalog=SOF102;Integrated Security=True";
+        private SqlConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+            connection = new SqlConnection(connectionString);
+     
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string query = "SELECT MaBaoCao, NgayBaoCao, DoanhThu, LoaiBaoCao FROM BaoCao WHERE NgayBaoCao >= DATEADD(MONTH, -1, GETDATE())";
+            LoadDataToGridView(query);
+        }
+        private void LoadDataToGridView(string query)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                connection.Close();
+
+                dgvThongKe.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi tải dữ liệu: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+        }
+
+        private void btnFIller_Click(object sender, EventArgs e)
+        {
+            string filter = cmbTimeFilter.SelectedItem.ToString();
+            string query = "SELECT MaBaoCao, NgayBaoCao, DoanhThu, LoaiBaoCao FROM BaoCao WHERE 1=1";
+
+
+            if (filter == "Hôm nay")
+                query += " AND CAST(NgayBaoCao AS DATE) = CAST(GETDATE() AS DATE)";
+            else if (filter == "Tuần này")
+                query += " AND NgayBaoCao >= DATEADD(DAY, -7, GETDATE())";
+            else if (filter == "Tháng này")
+                query += " AND NgayBaoCao >= DATEADD(MONTH, -1, GETDATE())";
+
+            LoadDataToGridView(query);
+        }
+    }
+}
